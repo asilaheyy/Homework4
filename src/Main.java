7a9499a07a8887a1e82c2cc966e27fe20ef4fe83
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-08-29 13:45:10 +0300

    Сделано ДЗ н.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Домашнее задание 1
+        //1.
+        int age = 18;
+            if (age>= 18) {
+                System.out.println("Поздравляем с совершеннолетием!");
+            }
+            if (age<18){
+                System.out.println("Вы не достигли совершеннолетнего возраста.");
+            }
+
+
     }
 }
