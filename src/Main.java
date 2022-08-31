7e4a9910530988e2e93f2ac01a0cc88c7bb31cbe
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-08-31 10:36:07 +0300

    Сделано дз 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //Домашнее задание 1
+        //Домашнее задание 1,2
         //1.
         int age = 18;
             if (age>=18) {
@@ -44,6 +44,47 @@ public class Main {
              }
             else {
         System.out.println("В вагоне остались места");
+            }
+
+            //Домашнее задание 3
+            //1.
+        int PersonAge =18;
+            if(PersonAge>=2 && PersonAge<=6){
+                System.out.println("Если возраст человека равен"+PersonAge+", то ему нужно ходить в садик.");
+            }
+            else if (PersonAge>=7 && PersonAge<=18){
+                System.out.println("Если возраст человека равен "+PersonAge+", то ему нужно ходить в школу.");
+            }
+            else if (PersonAge>18 && PersonAge<24){
+                System.out.println("Если возраст человека равен "+PersonAge+", то ему нужно ходить в университет.");
+            }
+            else {
+                System.out.println("Если возраст человека равен "+PersonAge+", то ему нужно ходить на работу.");
+            }
+
+            //2.
+        int ageForAttraction =8;
+            if(ageForAttraction<5){
+                System.out.println("Ребенок не может кататься на аттракционе.");
+            }
+            else if (ageForAttraction>5 && ageForAttraction<14){
+                System.out.println("Ребенок может кататься на аттракционе в сопровождении взрослого.");
+            }
+            else {
+                System.out.println("Ребенок может кататься без сопровождения.");
+            }
+            //3.
+            int one=57;
+            int two=9;
+            int three=83;
+            if(one>two && one>three){
+                System.out.println("Самое большее из чисел: "+one);
+            }
+            else if(two>one && two>three){
+                System.out.println("Самое большее из чисел: "+two);
+            }
+            else if(three>one && three>two){
+                System.out.println("Самое большее из чисел:"+three);
             }
     }
 }
