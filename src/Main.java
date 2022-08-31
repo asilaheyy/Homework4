ce33f05ed2409c80d441cede42d41d8549c3bdce
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2022-08-31 09:56:57 +0300

    Сделано дз 2

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,24 @@ public class Main {
         //Домашнее задание 1
         //1.
         int age = 18;
-            if (age>= 18) {
+            if (age>=18) {
                 System.out.println("Поздравляем с совершеннолетием!");
             }
-            if (age<18){
+            else {
                 System.out.println("Вы не достигли совершеннолетнего возраста.");
             }
         //2.
-        int personAge =17;
-            if (personAge>=7){
+        int personAge =24;
+            if (personAge>= 7 && personAge <18){
                 System.out.println("Вы в школе.");
             }
-            if (personAge>=18){
-                System.out.println("Вы окончили школу и можете поступать в университет.");
-            }
-            if (personAge>=24){
-                System.out.println("Вы окончили университет и можете идти на работу.");
+
+           else if (personAge>=18 && personAge <24){
+        System.out.println("Вы можете поступать в университет.");
+             }
+
+            else{
+                System.out.println("Вы можете идти на работу.");
             }
             //3.
 
@@ -28,19 +30,19 @@ public class Main {
             if (seats==0){
         System.out.println("В вагоне не осталось сидячих мест.");
             }
-            if (seats>=1){
+            else {
                 System.out.println("В вагоне есть сидячие места.");
             }
             if (standing ==0){
                 System.out.println("В вагоне не осталось стоячих мест.");
             }
-            if (standing>=1){
+            else {
         System.out.println("В вагоне остались стоячие места.");
             }
             if (placesInWagon ==0){
         System.out.println("В вагоне не осталось мест.");
              }
-            if (placesInWagon >=1){
+            else {
         System.out.println("В вагоне остались места");
             }
     }
